67e2e9a737b223977fabd634c6507be5c1e0401b
AlexnderDoroshenko <doroshenkoaldm@gmail.com>
2021-05-16 10:41:23 +0300

    Created ConfigLoader class in config.py, added field data_loader into Config class.

--- src/configuration/config.py
+++ src/configuration/config.py
@@ -13,12 +13,36 @@ class AbstractConfig(ABC):
     def set_config(self, key: str, value):
         raise NotImplemented
 
-
 class Config(AbstractConfig):
     """Main config class"""
     def __init__(self, input_data):
         self.input_data = input_data
+        self.data_loader = ConfigLoader(self.input_data)
 
     def set_config(self, key: str, value):
         self.__setattr__(name=key, value=value)
 
+
+class ConfigLoader:
+    def __init__(self, file: str):
+        self.file = file
+
+    def get_input_data(self):
+        if os.path.isfile(self.file):
+            if self.file.endswith(".yaml"):
+                return self.get_yaml_data()
+            if self.file.endswith(".json"):
+                return self.get_json_data()
+            else:
+                raise ValueError("Unsupported file extension")
+        else:
+            raise FileNotFoundError
+
+
+    def get_yaml_data(self):
+        with open(self.file) as f:
+            return yaml.safe_load(f)
+
+    def get_json_data(self):
+        with open(self.file).read() as f:
+            return json.loads(f)
